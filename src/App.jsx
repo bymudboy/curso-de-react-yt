d6c7d79eda88fbd38601f7f3aa76096da44e3ef5
bymudboy <luis.high@hotmail.com>
2025-05-29 16:59:00 -0400

    comit inicial

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import AddTask from "./components/AddTask";
+import Tasks from "./components/tasks";
+import { useEffect, useState } from "react";
+import {v4} from "uuid";  
+import Title from "./components/Title";
+
+function App() {
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || [] 
+);
+
+
+useEffect(() => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}, [tasks]);
+
+useEffect(() => { // caso queira pegar as tareafas de uma api
+  // const fetchTasks = async () => {
+     // chama api
+  // const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10", {method: "GET"});
+     // pegar os dados
+  // const data = await response.json();
+     // armazenar e persistir os dados no state
+  // setTasks(data);
+  // }
+  // fetchTasks();
+
+}, []);
+
+
+function onTaskClick(taskId) {
+  const newTasks = tasks.map((task) => {
+    if (task.id === taskId) {
+      return { ...task, isCompleted: !task.isCompleted };
+    }
+    // NÃO PRECISO ATUALIZAR ESSA TAREFA
+    return task;
+  });
+  setTasks(newTasks);
+}
+
+function onDeleteTaskClick(taskId) {
+  const newTasks = tasks.filter((task) => task.id !== taskId);
+  setTasks(newTasks);
+
+}
+
+function onAddTaskSubmit(title, description) {
+  const newTask = {
+    id: v4(),
+    title,
+    description,
+    isCompleted: false,
+  };
+  setTasks([...tasks, newTask]);
+}
+
+  return (
+    
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <Title>Gerenciador de Tarefas</Title>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
+        <Tasks tasks={tasks} 
+        onTaskClick={onTaskClick}
+        onDeleteTaskClick={onDeleteTaskClick}
+        
+        /> 
+      </div>
+    </div>
+
+    );
+}
+
+
+export default App;
